0144cd7650ad1d86300698e210ce51dabfaa57c6
Zoin <zoinen@gmail.com>
2026-08-19 22:21:59 +0300

    ci: force static Go executable mode

--- ci/build-portable-qt-linux.sh
+++ ci/build-portable-qt-linux.sh
@@ -208,6 +208,7 @@ go test -tags f4_embedded_qt_host \
     -run 'TestMaterializeEmbeddedQtHost|TestGeneratedEmbeddedQtHostPayload' .
 mkdir -p dist/f4-linux-amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath \
+    -buildmode=exe \
     -tags f4_embedded_qt_host -ldflags='-s -w' \
     -o dist/f4-linux-amd64/f4 .
 bash ci/audit-static-go-linux.sh dist/f4-linux-amd64/f4
